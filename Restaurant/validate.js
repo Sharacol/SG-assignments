de1bd711f13945150372def0c0c013f0fe02b5b0
Sharacol Marcellino <semarcellino@gmail.com>
2018-12-17 21:18:52 -0500

    adding rough draft

--- Restaurant/validate.js
+++ Restaurant/validate.js
@@ -1,25 +1,54 @@
 let contactSubmit = () =>{
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let phone = document.getElementById('phone').value
-    let inquiry = document.getElementById('inquiry')
-    let additionalInfo = document.getElementById('additionalInfo').value
-    let visit = document.getElementsByName('visit')
-    let contact = document.getElementsByName('days')
+    let information ={
+        " Name": document.getElementById('name').value,
+        " Email": document.getElementById('email').value,
+        " Phone": document.getElementById('phone').value,
+        " Inquiry": document.getElementById('inquiry').value,
+        " Additional information": document.getElementById('additionalInfo').value,
+        " Visited": document.getElementsByName('visit'),
+        " Day(s) to contact": document.getElementsByName('days'),
+    }
+
 
     let visited
-    visit.forEach((option)=>{
+    information[" Visited"].forEach((option)=>{
         if(option.checked){
             visited= option.value
         }
     })
-let daysVisited = []
-contact.forEach((day)=>{
+    let daysContact = []
+    information[" Day(s) to contact"].forEach((day)=>{
         if(day.checked){
-            daysVisited.push(day.value)
+            daysContact.push(day.value)
         }
     })
-    console.log(inquiry)
+
+    let missingInfo=[]
+    
+    for(let info in information){
+        if(info === ' Day(s) to contact'){
+            if(daysContact.length > 0){
+                continue
+            }
+            missingInfo.push(info)
+        }else if(info === ' Visited'){
+            if(visited){
+                continue
+            }else{
+                missingInfo.push(info)
+            }
+        }else if(information[info]){
+            continue            
+        }else{
+            missingInfo.push(info)
+        }
+    }
+
+    if(missingInfo.length===0){
+        alert('Thank you! We will get backto you soon.')
+    }else{
+        alert(`Missing: ${missingInfo}`)
+    }
 
     return false
 }
